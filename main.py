82ed277bdc37dd61452c83cc61a8ab1d2ce1ae3c
esraaaatmeh <falahmustafa751@gmail.com>
2023-10-28 15:47:18 +0300

    addm

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
-print('hello world')
+# print('hello world')
+
+# #1 string
+# name = ''' mustafa 
+# ali 
+# ahmad'''
+# print (name)
+
+
+# #2 int
+# nom =1
+# print (nom)
+
+
+# #3 flote
+# nom1 =1.2345678
+# print (nom)
+
+# #4 polian
+
+
+# #5 list
+# list1 = [1,3,4,2,4,5,5,6]
+# print (list1)
+
+# #6 tuple
+# tp = (1,3,3,4,5,6,7,78)
+# print (tp)
+
+# #7 dictionary : {key: value}
+# parsone = {'name': 'mustafa',
+# 'age':'20',
+#   'jop': 'devlobar',
+#    'skils': ['java','python','html','css']} 
+#    print (parsone)
+
+
+
+# for with in 
+parsone = {'name': 'mustafa',
+'age':'20',
+'jop': 'devlobar',
+'skils': ['java','python','html','css']} 
+print (parsone)
+for i in parsone:
+    print(i,':',parsone[i] )
+
+n=50
+for i in range(0,n,2):
+print(i)
+
+
