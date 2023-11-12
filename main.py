8d3abf6b63188d0e8424ff39dd579564bdd64284
Mustafa-alkeyam <falahmustafa751@gmail.com>
2023-11-12 21:04:25 +0200

    add login

--- main.py
+++ main.py
@@ -69,38 +69,103 @@
 #         i+=1
 #         break
 
-# allperson = []
-# person = {}
-# print("welcome to uor app")
-# while True:
-#     print('__'*20)
-#     print('1 - add new person')
-#     print('2 - see all person')
-#     print('3 - exit')
-#     choice = input('enter your choice')
-#     if choice == '1':
+allperson = []
+person = {}
+print("welcome to uor app")
+while True:
+    print('__'*20)
+    print('1 - create account')
+    print('2 - login')
+    print('3 - exit')
+    choice = input('enter your choice')
+    if choice == '1':
 
-#         name = input('enter the person name :')
-#         age = input('enter the person age :')
-#         jop = input('enter the person jop :')
-#         person['name'] = name
-#         person['age'] = age
-#         person['jop'] = jop
-#         allperson.append(person)
-#         print("you added person successfuly")
-#     elif choice == '2':
-#         counter = 1
-#         print("__"*20)
+        name = input('enter the user name :')
+        if allperson != []:
+            isExited = True
+            while isExited:
+                for i in allperson:
+                    if name == i['name']:
+                        print('this name is already exist')
+                        name = input('enter the user name :')
+                        isExited = True
+                        break
+                    else:
+                        isExited = False
+        
+        while True:
+            try:
+                age = int(input('enter the user age : '))
 
-#         print('all person : ')
-#         for i in allperson:
-#             print('person ',counter)
-#             for key in i:
-#                 print('     ', key, ':',i[key])
-#             counter+=1
+                break
+            except Exception as e:
+                print(e)
+                continue
 
-#     elif choice == '3':
-#         break
+
+        
+        jop = input('enter the user jop :')
+        skills = []
+        while True:
+            try:
+                counter2 = int(input('enter the number of skills : '))
+
+                break
+            except Exception as e:
+                print('please enter a valid number')
+                continue
+        for i in range(counter2):
+            print('enter the skills  ',i + 1,':')
+            skills.append(input())
+
+        pasword = input('enter the pasword : ')
+
+        person={
+            'name' : name,
+            'age' : age,
+            'jop' : jop,
+            'pasword' : pasword,
+            'skills' : skills
+
+        }
+        
+        allperson.append(person)
+        print(allperson)
+        print("you added person successfuly")
+
+    elif choice == '2':
+        # counter = 1
+        # print("__"*20)
+
+        # print('all person : ')
+        # for i in allperson:
+        #     print('person ',counter)
+        #     for key in i:
+        #         print('     ',key,':',i[key])
+        #     counter+=1
+        print("__"*20)
+        print('please inter your name and pasword: ')
+        name = input('enter the user name: ')
+        pasword = input('enter the password: ')
+
+        for i in allperson:
+            if i['name']== name and i['pasword']== pasword:
+                print('welcome ',name)
+                break
+        else:
+            print('the name or password is wrong')
+
+    elif choice == '3':
+        print('thank you for using our app')
+        break
+    elif choice == '4':
+        print(allperson)
+    else:
+        print('wrong choice')
+
+
+
+
 # list1 =['a','s','e','y','j','6','9']
 # list1.sort()
 # print(list1)
